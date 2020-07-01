e4c838819297370d07a0c9ef24a6b3f60a99709d
SlayerShadow <gawfast@gmail.com>
2020-07-01 22:11:18 +0300

    Add comment with TCP rendering for snakes.

--- examples/snake_game/game_engine/interface.cr
+++ examples/snake_game/game_engine/interface.cr
@@ -1,8 +1,13 @@
 require "tilerender"
 require "tilerender/interfaces/command_line"
+# To have render over TCP comment previous and uncomment this
+# require "tilerender/interfaces/tcp"
 
 module Game
 
+	# Tilerender interface customization.
+	# To have render over TCP comment previous and uncomment this:
+	# class Interface < Tilerender::TCP
 	class Interface < Tilerender::CommandLine
 		def wall( x : UInt16, y : UInt16 ) : Void
 			background x, y, :white
